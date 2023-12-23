95cdda36e3523890152ccabc423b8c6aaf41b439
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-23 18:18:03 +0800

    Moved some files and modified some configs

--- commands/reinit_file.rb
+++ commands/reinit_file.rb
@@ -2,6 +2,11 @@
 
 require('fileutils')
 
+LIBS_TO_INSTALL = %w[
+  solargraph
+  rubocop
+].freeze
+
 Dir.chdir('/home/playground')
 
 puts 'Removing all files in the playground folder...'
@@ -19,5 +24,18 @@ end
 puts 'Initializing bundler project...'
 puts 'Copying assets to the playground folder...'
 FileUtils.cp_r('/app/assets/.', '.')
+src_file = File.open('main.rb', 'w+')
+src_file.puts '# frozen_string_literal: true'
+src_file.puts ''
+src_file.puts 'puts \'Hello World!\''
+src_file.close
+
+puts 'Setting up bundler...'
+system('bundle init')
+
+LIBS_TO_INSTALL.each do |lib|
+  system("bundle add #{lib} -g development --skip-install")
+end
+
 system('bundle install')
 puts 'Done!'
